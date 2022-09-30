ed3d39d8beebaea36ffba9cc54f46bc0d20658f5
Dhruv Gupta <101388885+Dhruva8878@users.noreply.github.com>
2022-09-30 16:33:57 +0530

    added
    added

--- Covid_app/src/main/java/com/covid/model/VaccineInventory.java
+++ Covid_app/src/main/java/com/covid/model/VaccineInventory.java
@@ -4,13 +4,34 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VaccineInventory {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer vaccineInventoryid;
 	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern="dd/MM/yyyy")
+	@NotNull(message="Date is required")
 	private LocalDate date;
 	
 	@OneToOne(cascade = CascadeType.ALL ,mappedBy = "vaccineInventory")
--- Covid_app/src/main/java/com/covid/model/VaccineRegistration.java
+++ Covid_app/src/main/java/com/covid/model/VaccineRegistration.java
@@ -9,12 +9,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VaccineRegistration {
 	
 	@Id
@@ -22,6 +30,7 @@ public class VaccineRegistration {
 	private Integer regId;
 	
 	@Size(max=10 , message="Moblie Number length should be 10!")
+	@Pattern(regexp = "^[6-9]{1}[0-9]{9}$", message = "Mobile number is invalid")
 	private Long mobileNo;
 	
     @JsonFormat(pattern = "dd/MM/yyyy")
